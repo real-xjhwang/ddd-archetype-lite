5bd78fd6adc7c31b2eb34093f41befc25d39e075
黄雪杰 <xjhwang@163.com>
2024-07-10 18:13:41 +0800

    添加常用的工具类

--- ddd-archetype-lite-types/src/main/java/com/xjhwang/util/ArrayUtils.java
+++ ddd-archetype-lite-types/src/main/java/com/xjhwang/util/ArrayUtils.java
@@ -1,15 +1,13 @@
 package com.xjhwang.util;
 
-import cn.hutool.core.util.IdUtil;
+import cn.hutool.core.util.ArrayUtil;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * ID工具类
- *
- * @author 黄雪杰 on 2024-06-11 16:47
+ * @author 黄雪杰 on 2024-07-10 18:12
  */
 @Slf4j
 @UtilityClass
-public class IdUtils extends IdUtil {
+public class ArrayUtils extends ArrayUtil {
 }
--- ddd-archetype-lite-types/src/main/java/com/xjhwang/util/HttpUtils.java
+++ ddd-archetype-lite-types/src/main/java/com/xjhwang/util/HttpUtils.java
@@ -1,15 +1,13 @@
 package com.xjhwang.util;
 
-import cn.hutool.core.util.IdUtil;
+import cn.hutool.http.HttpUtil;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * ID工具类
- *
- * @author 黄雪杰 on 2024-06-11 16:47
+ * @author 黄雪杰 on 2024-07-10 18:13
  */
 @Slf4j
 @UtilityClass
-public class IdUtils extends IdUtil {
+public class HttpUtils extends HttpUtil {
 }
